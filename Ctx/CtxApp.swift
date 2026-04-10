07d8d9a64a21f172b573e28be049bf644a900ed4
Sam Lee <samlee616@gmail.com>
2026-04-10 18:30:56 -0400

    Initial Commit

---
+++ Ctx/CtxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CtxApp.swift
+//  Ctx
+//
+//  Created by Samantha Lee on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct CtxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
